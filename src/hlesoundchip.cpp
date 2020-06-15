85ca91dafef9e8717485e172e8e8cb8e59298a4c
tildearrow <tildearrow@protonmail.com>
2020-06-14 20:47:03 -0500

    yep

---
+++ src/hlesoundchip.cpp
@@ -0,0 +1,24 @@
+#include "soundchip.h"
+#include <string.h>
+
+#ifdef _WIN32
+#define M_PI 3.141592653589793238
+#endif
+
+void HLESoundchip::NextSample(float* l, float* r) {
+  *l=0;
+  *r=0;
+}
+
+void HLESoundchip::Init() {
+  Reset();
+  for (int i=0; i<65280; i++) {
+    pcm[i]=0;
+  }
+}
+
+void HLESoundchip::Reset() {
+  for (int i=0; i<8; i++) {
+  }
+  memset(chan,0,sizeof(channel)*8);
+}
